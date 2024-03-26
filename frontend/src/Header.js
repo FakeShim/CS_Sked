b9676cd6c3b895e31cd69763b5f5ac371f0c8864
david-dillard <dldillard@crimson.ua.edu>
2024-03-25 19:23:25 -0500

    Removing volume overwrite in docker-compose

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -5,7 +5,6 @@ const Header = () => {
   return (
     <header className="header">
       <div className="header-left">
-        {/* Wrap the title and image with Link components */}
         <Link to="/" className="header-link">
           <img src="ua_logo.png" alt="Header Image" className="ua-logo" />
           <h1 className="header-title">Scheduler</h1>
